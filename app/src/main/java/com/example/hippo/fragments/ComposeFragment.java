b7c9bbd63b3051c30d0bd73d429406424cb25c7a
Edimar Valentín Kery <edimar.valentin@upr.edu>
2022-04-24 21:23:24 -0400

    Improved the compose fragment view

--- app/src/main/java/com/example/hippo/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/hippo/fragments/ComposeFragment.java
@@ -16,10 +16,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -44,8 +47,14 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class ComposeFragment extends Fragment {
+import kotlin.jvm.internal.CollectionToArray;
+
+public class ComposeFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     public static final String TAG = "ComposeFragment";
+
+    public static final String[] reminders = new String[]{"Never", "10 minutes before", "30 minutes before", "1 hour before",
+            "2 hours before", "1 day before", "2 days before", "1 week before", "2 weeks before", "1 month before"};
+
     public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
     DatePickerDialog datePicker;
     TimePickerDialog timePicker;
@@ -53,17 +62,15 @@ public class ComposeFragment extends Fragment {
     private EditText etTitle;
     private EditText etDate;
     private EditText etTime;
-    private EditText etReminderTime;
-    private ImageView ivTaskImage;
-    private File photoFile;
+
+    private Spinner spinReminder;
+
+    private ArrayAdapter<String> spinAdapter;
 
     private Button btnAdd;
-    private Button btnCaptureImage;
 
-    public String photoFileName = "photo.jpg";
-    private  String amPm;
-
-    public ComposeFragment(){}
+    public ComposeFragment() {
+    }
 
     @Nullable
     @Override
@@ -74,14 +81,16 @@ public class ComposeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        etTaskDescription = view.findViewById(R.id.etTaskDescription);
-        etTitle = view.findViewById(R.id.etTitle);
-        etDate = view.findViewById(R.id.etDate);
-        etTime = view.findViewById(R.id.etTime);
-        ivTaskImage = view.findViewById(R.id.ivTaskImage);
-        etReminderTime = view.findViewById(R.id.etReminderTime);
-        btnCaptureImage = view.findViewById(R.id.btnCaptureImage);
-        FloatingActionButton btnAdd =  view.findViewById(R.id.btnAdd);
+        etTaskDescription = view.findViewById(R.id.etCompose_task_description);
+        etTitle = view.findViewById(R.id.etCompose_task_title);
+        etDate = view.findViewById(R.id.etCompose_task_date);
+        etTime = view.findViewById(R.id.etCompose_task_time);
+        btnAdd = view.findViewById(R.id.btnCompose_task_create);
+        spinReminder = view.findViewById(R.id.spinCompose_task_reminder);
+
+        spinAdapter = new ArrayAdapter<>(getContext(), R.layout.hippo_spinner_item, reminders);
+        spinReminder.setAdapter(spinAdapter);
+        spinReminder.setOnItemSelectedListener(this);
 
         etDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,9 +104,9 @@ public class ComposeFragment extends Fragment {
                 datePicker = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        etDate.setText(dayOfMonth + "/" + (monthOfYear+1) + "/" + year);
+                        etDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
                     }
-                }, year,month,day);
+                }, year, month, day);
                 datePicker.show();
             }
         });
@@ -111,29 +120,20 @@ public class ComposeFragment extends Fragment {
                 timePicker = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int pHour, int pMinute) {
-                        if(pHour>=12){
-                            amPm = "PM";
+                        if (pHour >= 12) {
+                            //  amPm = "PM";
+                        } else {
+                            // amPm = "AM";
                         }
-                        else{
-                            amPm = "AM";
-                        }
-                        etTime.setText(pHour + ":" + pMinute + " " + amPm);
-                        etReminderTime.setText(pHour-2+":"+pMinute+ amPm);
+                        //etTime.setText(pHour + ":" + pMinute + " " + amPm);
+                        // etReminderTime.setText(pHour-2+":"+pMinute+ amPm);
                     }
                 }, hour, minutes, false);
                 timePicker.show();
             }
         });
 
-        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchCamera();
-            }
-        });
-
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
+/*        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ParseUser currentUser = HippoUser.getCurrentUser();
@@ -141,7 +141,7 @@ public class ComposeFragment extends Fragment {
                 String Description = etTaskDescription.getText().toString();
                 String Date = etDate.getText().toString();
                 String Time = etTime.getText().toString();
-                String Reminder = etReminderTime.getText().toString();
+              //  String Reminder = etReminderTime.getText().toString();
                 SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 
 
@@ -173,59 +173,6 @@ public class ComposeFragment extends Fragment {
             }
         });
     }
-
-    private void launchCamera() {
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference for future access
-        photoFile = getPhotoFileUri(photoFileName);
-
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileproviderHippo", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                // by this point we have the camera photo on disk
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                // RESIZE BITMAP, see section below
-                // Load the taken image into a preview
-                ivTaskImage.setImageBitmap(takenImage);
-            } else { // Result was a failure
-                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private File getPhotoFileUri(String photoFileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(TAG, "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + photoFileName);
-
-        return file;
-    }
-
     private void SaveTask(ParseUser currentUser, String title, String description, Date dueTime, Date reminderTime) {
         Task task = new Task();
         task.put(Task.KEY_USER,currentUser);
@@ -234,7 +181,7 @@ public class ComposeFragment extends Fragment {
         task.setDueTime(dueTime);
         task.setTitle(title);
         task.setReminder(reminderTime);
-        task.setAttachment(new ParseFile(photoFile));
+       // task.setAttachment(new ParseFile(photoFile));
         task.saveInBackground(new SaveCallback() {
             @Override
             public void done(com.parse.ParseException e) {
@@ -251,6 +198,17 @@ public class ComposeFragment extends Fragment {
                 ivTaskImage.setImageResource(0);
             }
         });
+    }*/
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
 
